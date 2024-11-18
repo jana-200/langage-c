835c60a2c8407644f8a6666a672854c97353eddb
jana-200 <kamalgana2004@gmail.com>
2024-11-18 14:01:24 +0100

    ex1.c

--- TP5/ex1.c
+++ TP5/ex1.c
@@ -4,25 +4,28 @@
 #include <ctype.h>
 
 int main(int argc, char *argv[]){ 
-    char* maj[argc-1];
+
+    char **maj = (char**)malloc((argc-1)*sizeof(char*));
+    if (maj == NULL) exit(1);
 
     for(int i=1 ; i<argc; i++){ 
-        size_t len= strlen(argv[i]);
-        maj[i-1]= malloc((len + 1) * sizeof(char));
-        if (maj[i - 1] == NULL) {
-            exit(1);
-        }
 
-        for(size_t  j=0;j<len;j++){ 
+        size_t len= strlen(argv[i]); // len il prend pas le \0
+        maj[i-1]= malloc((len + 1) * sizeof(char));
+
+        if (maj[i - 1] == NULL) exit(1);
+
+        for(size_t j=0 ;j<len;j++){ 
             maj[i-1][j]=toupper(argv[i][j]);
         }
 
         maj[i - 1][len] = '\0';
-        
     }
 
     for (int i = 0; i < argc - 1; i++) {
         printf("%s ", maj[i]);
+        printf("\n");
     }
+
     printf("\n");
 }
