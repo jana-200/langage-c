6aeda512d535fc9d454a85cf029d84aef3536faf
jana-200 <kamalgana2004@gmail.com>
2024-11-25 11:44:44 +0100

    pas correct

--- TP5/ex3.c
+++ TP5/ex3.c
@@ -4,5 +4,43 @@
 
 #define TAILLE 254
 
-int main(){
+int main() {
+    char ligne[TAILLE];
+    char **coffres = NULL;
+    int nb_coffres = 0;
+
+    while (fgets(ligne, sizeof(ligne), stdin) != NULL) {
+        int trouve = 0;
+        
+        for (int i = 0; i < nb_coffres; i++) {
+            if (strncmp(coffres[i], ligne, 5) == 0) {
+                coffres[i] = realloc(coffres[i], (strlen(coffres[i]) + strlen(ligne+5) +1) * sizeof(char));
+                if (coffres[i] == NULL) exit(1);
+                strcat(coffres[i], ligne+5);
+                trouve = 1;
+                break;
+            }
+        }
+
+        if (!trouve) {
+            coffres = realloc(coffres, (nb_coffres + 1) * sizeof(char*));
+            if (coffres == NULL) exit(1);
+
+            coffres[nb_coffres] = malloc((strlen(ligne) + 1) * sizeof(char));
+            if (coffres[nb_coffres] == NULL) exit(1);
+
+            for(int i = 0; i < strlen(ligne); i++) {
+                coffres[nb_coffres][i] = ligne[i];
+            }
+            nb_coffres++;
+        }
+    }
+
+    for (int i = 0; i < nb_coffres; i++) {
+        printf("%s\n", coffres[i]);
+        free(coffres[i]); 
+    }
+    free(coffres); 
+
+    exit(0);
 }
