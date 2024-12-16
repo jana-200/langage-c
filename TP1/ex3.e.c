fb7d05d7e72918d0663af62522bc5138496a52f5
jana-200 <kamalgana2004@gmail.com>
2024-12-16 01:39:52 +0100

    <3

--- TP1/ex3.e.c
+++ TP1/ex3.e.c
@@ -9,7 +9,7 @@ int main(){
 
     int fact=1;
     int i=2;
-    while( i <= x){ 
+    while(i <= x){ 
         if(fact> INT_MAX/i){
             printf("Error : integer overflow\n");
             exit(EXIT_FAILURE);
--- TP2/ex1.c
+++ TP2/ex1.c
@@ -6,15 +6,13 @@
 int main() {
     float tab[MAX];
     float val;
-    int taille = 0;
     float somme = 0;
     int e = 0;
 
-    printf("Entrez les notes des etudiants (Ctrl+Z pour terminer) :\n");
+    printf("Entrez les notes des etudiants (Ctrl+D pour terminer) :\n");
     while (scanf("%f", &val) != EOF && e < MAX) {
         tab[e] = val;
         e++;
-        taille++;
         somme += val;
         if (e == MAX)
             break;
@@ -22,7 +20,7 @@ int main() {
 
     printf("Moyenne = %f \n", somme / e);
     printf("Ecarts des etudiants par rapport à la moyenne : \n");
-    for (int i = 0; i < taille; i++) {
+    for (int i = 0; i < e; i++) {
         printf(" Etudiant %d: %f - %f = %f\n", i + 1, tab[i], somme / e, tab[i] - (somme / e));
     }
 
